63e83d20a8926e2e4020d35a4ea346527ec09cd8
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-08-11 21:16:20 +0300

    LeetCode

---
+++ 946. Validate Stack Sequences/validate-stack-sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        j = 0
+        N = len(popped)
+        for x in pushed:
+            stack.append(x)
+            while stack and j < N and stack[-1] == popped[j]:
+                j += 1
+                stack.pop()
+        return j == len(popped)
+        
