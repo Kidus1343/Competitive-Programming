fd0ac45a84114fdf95fb302de3769f97910611e5
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-08-24 18:31:07 +0300

    40 ms, faster than 70.96% LeetCode

---
+++ 394. Decode String/decode_string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        nums = [x for x in range(0, 10)]
+        stack = []
+        
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                subStr = ""
+                while stack and stack[-1] != "[":
+                    subStr = stack.pop() + subStr
+                stack.pop()
+                
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k) * subStr)
+        return "".join(stack)
+            
+            
+                
+                
