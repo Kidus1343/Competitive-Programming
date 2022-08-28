49c04e937942a9f16d2c83465cfc01853210be4f
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-08-28 21:03:56 +0300

    LeetCode

---
+++ 486. Predict the Winner/predict_winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        def dfs(i, j):
+            if i == j:
+                return nums[i]
+
+            pick_left = nums[i] - dfs(i+1, j)
+            pick_right = nums[j] - dfs(i, j-1)
+            
+            return max(pick_left, pick_right)
+        
+        return dfs(0, n-1) >= 0
+        
